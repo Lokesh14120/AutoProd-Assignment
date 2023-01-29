5e03c9f68b7414376fcef71c546a98ac99c2782a
Lokesh14120 <lokeshyadav1808@gmail.com>
2023-01-29 10:53:08 +0530

    initialised both the front and the back

---
+++ backend/api/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+
+# Create your views here.
+
+
+def getRoutes(request):
+
+    return JsonResponse('our api',safe=False)
---
+++ backend/frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      this is the form filling
+    </div>
+  );
+}
+
+export default App;
