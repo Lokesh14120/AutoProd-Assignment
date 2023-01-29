da65c8c2fb441c7a4a47d7caf19d2ccf2c809749
Lokesh14120 <lokeshyadav1808@gmail.com>
2023-01-29 13:21:57 +0530

    some changes

--- backend/frontend/src/App.js
+++ backend/frontend/src/App.js
@@ -6,11 +6,15 @@ function App() {
   const [selectedValue2, setSelectedValue2] = useState("");
   const [selectedValue3, setSelectedValue3] = useState("");
   const [selectedValue4, setSelectedValue4] = useState("");
+  const [outputvalue, setoutputvalue] = useState("hi");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
    
     console.log(selectedValue, selectedValue2, selectedValue3, selectedValue4);
+    
+
+
   };
 
   return (
@@ -18,6 +22,7 @@ function App() {
     <div className='Applogo'>
       <h1> Welcome to AutoProd</h1>
     </div>
+    <div><h3>Choose among the following and we will tell something about you.</h3></div>
     <div className='form-container'>
       <form className='form' onSubmit={handleSubmit}>
         <div className="grid-container">
@@ -42,7 +47,7 @@ function App() {
           </div>
           <div className='form-group'>
             <label>Pick your favorite animal:</label>
-            <select className='form-control' defaultValue='' onChange={(e) => setSelectedValue3(e.target.value)}>
+            <select className='form-control' defaultValue="" onChange={(e) => setSelectedValue3(e.target.value)}>
               <option value="" disabled>Select an animal</option>
               <option value="cat">Cat</option>
               <option value="dog">Dog</option>
@@ -50,19 +55,25 @@ function App() {
             </select>
           </div>
           <div className='form-group'>
-            <label>Pick your favorite animal:</label>
-            <select className='form-control' defaultValue='' onChange={(e) => setSelectedValue4(e.target.value)}>
-              <option value="" disabled>Select an animal</option>
-              <option value="cat">Cat</option>
-              <option value="dog">Dog</option>
-              <option value="bird">Bird</option>
+            <label>Pick your favorite food:</label>
+            <select className='form-control' defaultValue="" onChange={(e) => setSelectedValue4(e.target.value)}>
+              <option value="" disabled>Select type of food</option>
+              <option value="Vegiterian">Vegiterian</option>
+              <option value="Vegan">Vegan</option>
+              <option value="Non Vegiterian">Nog Vegiterian</option>
             </select>
           </div>
         </div>
         <div className='form-group'>
           <input className='form-control' type="submit" value="Submit" />
         </div>
+
       </form>
+           <div className='output'>
+            <h4>Here is something about you</h4>
+          <textarea>{outputvalue}</textarea>
+
+          </div>
       </div>
     </div>
   );
